3085b7152047114024f64261bdde2b09114968dd
sialaichai <sialaichai@gmail.com>
2025-11-09 19:18:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const APPROVED_CALCULATOR_MAP = new Map([
     // --- Page 2: Current Scientific ---
     ['FX82MS', 'CASIO FX 82MS'],
     ['FX85MS', 'CASIO FX 85MS'],
-    ['CALCULATORFX95MS', 'CASIO FX 95MS'],
+    ['LATORFX95MS', 'CASIO FX 95MS'],
     ['FX96SGPLUS', 'CASIO FX 96SG Plus'],
     ['FX97SGX', 'CASIO FX 97SG X'],
     ['FX350MS', 'CASIO FX 350MS'],
@@ -460,6 +460,7 @@ function drawOverlay(words = []) {
 // --- 9. Start the App ---
 // This waits for the page to be loaded before running any code.
 window.addEventListener('DOMContentLoaded', initializeApp);
+
 
 
 
